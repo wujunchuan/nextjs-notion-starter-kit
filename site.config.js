d03ad1bdccfe5de279e7a3e84732dbf37aab77d3
JohnTrump <wujunchuan2008@gmail.com>
2021-02-09 16:14:12 +0800

    update domian

--- site.config.js
+++ site.config.js
@@ -8,7 +8,7 @@ module.exports = {
 
   // basic site info (required)
   name: "Mogic Time | Trump's blog",
-  domain: '',
+  domain: 'notion-blog-wujunchuan.vercel.app',
   author: 'John Trump',
 
   // open graph metadata (optional)
